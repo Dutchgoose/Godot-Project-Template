3ced2f672e0f00c725fa3463abd5b63a7028f0fb
gustav staprans <staprans@gmail.com>
2023-11-14 11:35:34 -0500

    remove everything

--- scenes/ui/sound_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _ready():
-	pressed.connect(on_pressed)
-	
-
-func on_pressed():
-	$RandomStreamPlayerComponent.play_random()
--- scenes/ui/vignette.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-
-func _ready():
-	GameEvents.player_damaged.connect(on_player_damaged)
-	
-
-func on_player_damaged():
-	$AnimationPlayer.play("hit")
