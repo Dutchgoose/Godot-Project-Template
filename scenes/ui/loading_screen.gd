e04a94dfe8a37be07e1cd15ea2c23b2039b579e7
gustav staprans <staprans@gmail.com>
2023-11-14 12:39:50 -0500

    Adding a comment to loading screen explaining the timer

--- scenes/ui/loading_screen.gd
+++ scenes/ui/loading_screen.gd
@@ -20,6 +20,8 @@ func _process(_delta: float):
 	scene_load_status = ResourceLoader.load_threaded_get_status(main_scene_path, loading_progress)
 	if (scene_load_status == ResourceLoader.THREAD_LOAD_LOADED):
 		main_scene = ResourceLoader.load_threaded_get(main_scene_path)
+		# In this template if you do not have an "await" here loading
+		# happens too fast and you can't see the loading logic at work visually.
 		await get_tree().create_timer(1).timeout
 		loading_label.visible = false
 		continue_button.visible = true
